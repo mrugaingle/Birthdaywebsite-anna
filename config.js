99a29e5c384d2d2f26f00466a358ce11ba805d2a
mrugaingle <mruga.ingle.2007@gmail.com>
2026-09-12 20:31:50 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -23,10 +23,11 @@ const CONFIG = {
     { file: "photo1.jpeg", caption: "25 September 2025: random moustache anna" },
     { file: "photo2.jpeg", caption: "Sunny Pahad" },
     { file: "photo3.jpeg", caption: "Anna for Sale" },
-    { file: "photo4.jpeg", caption: "Ganesh Chaturthi 2025: Love, Daisy" },
-    { file: "photo5.jpeg", caption: "birthday #22 apparently" },
+    { file: "photo4.jpeg", caption: "Ek Passport photo toh banta hai" },
+    { file: "photo5.jpeg", caption: "Ganesh Chaturthi 2025: Love, Daisy" },
     { file: "photo6.jpeg", caption: "we still don't know why we're laughing" },
-    { file: "photo7.jpg", caption: "" },
-    { file: "photo8.jpg", caption: "that one summer" },
+    { file: "photo7.jpeg", caption: "Gharghuti Ganesh Chathurti" },
+    { file: "photo8.jpeg", caption: "Sabka Anna" },
+    { file: "photo9.jpg", caption:"MAN'S 20 LESSGOOOOO"}
   ],
 };
